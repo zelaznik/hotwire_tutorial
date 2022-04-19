6776f8459b0175140aaf002f5c6d572de967692d
Steve Zelaznik <steve.zelaznik@gnar.dog>
2022-04-19 13:34:56 -0400

    build basic app with scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
